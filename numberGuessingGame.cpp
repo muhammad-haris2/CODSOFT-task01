ec48bcab7965e84bd5dee67926d2a806878b461e
Muhammad Haris <148313456+muhammad-haris2@users.noreply.github.com>
2024-03-28 20:38:56 +0500

    Add files via upload

---
+++ numberGuessingGame.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    int num, num1;
+    srand(time(0));
+    num = rand()%100;  //num is the random generated number
+    cout<<"Guess a number from 1 to 100"<<endl;
+    cout<<"Enter your guess number :";
+    cin>>num1;   //num1 is the number entered by the user
+
+    bool flag = true;
+    do{
+        if(num1<num){
+            cout<<"The number you guessed is less"<<endl;
+            cin>>num1;
+        }
+        else if(num1>num){
+            cout<<"The number you guessed is high"<<endl;
+            cin>>num1;
+        }
+        else if(num1==num){
+            cout<<"The number you guessed is correct"<<endl;
+            flag = false;
+        }
+
+    }
+    while(flag==true);
+    return 0;
+}
